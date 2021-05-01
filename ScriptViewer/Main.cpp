9c1dcf0fb07a0850ff2f24747ca139e03f437dff
pongo1231 <pongo1999712@gmail.com>
2021-05-01 03:42:52 +0200

    Remove unused forward declaration

--- ScriptViewer/Main.cpp
+++ ScriptViewer/Main.cpp
@@ -17,8 +17,6 @@
 
 #pragma endregion
 
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
 static ID3D11DeviceContext* ms_pDeviceContext = nullptr;
 
 static HWND ms_hWnd = 0;
